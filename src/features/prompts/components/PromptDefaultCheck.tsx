fa057b139e3eb1fd84d6cd63c4cf7520f6bad82a
Keita Katahira <katahirakeita1201@gmail.com>
2023-03-28 02:14:16 +0900

    事前指示のデフォルト設定を可能に

---
+++ src/features/prompts/components/PromptDefaultCheck.tsx
@@ -0,0 +1,31 @@
+import { useUpdatePrompt } from "@/features/prompts/hooks/useUpdatePrompt";
+import { Prompt } from "@/features/prompts/types/prompt";
+import { useNotification } from "@/shared/hooks/useNotification";
+import { Checkbox } from "@mantine/core";
+import { useCallback } from "react";
+
+type Props = {
+  prompt: Prompt;
+};
+
+export const PromptDefaultCheck = ({ prompt }: Props) => {
+  const { notifyError, notifySuccess } = useNotification();
+  const updatePromptMutation = useUpdatePrompt({ id: prompt.id });
+
+  const handleChange = useCallback(async () => {
+    console.log("aaa");
+    try {
+      await updatePromptMutation.mutateAsync({ isDefault: !prompt.isDefault });
+      notifySuccess({ message: "保存しました" });
+    } catch (e) {
+      notifyError({ message: "保存に失敗しました" });
+    }
+  }, [notifyError, notifySuccess, prompt.isDefault, updatePromptMutation]);
+  return (
+    <Checkbox
+      checked={prompt.isDefault}
+      onChange={handleChange}
+      styles={{ input: { cursor: "pointer" } }}
+    />
+  );
+};
