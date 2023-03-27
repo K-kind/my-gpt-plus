1fc0af2b64f16e124a1e13d509216b5f8f84adaa
Keita Katahira <katahirakeita1201@gmail.com>
2023-03-27 13:01:16 +0900

    functionsは削除

--- functions/src/constants/parameterizedConfigs.ts
+++
@@ -1,5 +0,0 @@
-import { defineString } from "firebase-functions/params";
-
-export const parameterizedConfigs = {
-  OPEN_AI_API_KEY: defineString("OPEN_AI_API_KEY"),
-};
--- functions/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { createChatCompletion } from "./createChatCompletion";
