71ef87e414ced30410efc50c8efccaf7ee7958c1
Keita Katahira <katahirakeita1201@gmail.com>
2023-04-01 10:55:55 +0900

    サーバーの不要なヘッダーを削除

--- backend/src/routes/api/streamChatCompletion.ts
+++ backend/src/routes/api/streamChatCompletion.ts
@@ -4,11 +4,9 @@ import { streamChatCompletion, validateParams } from "../../lib/openai";
 const router = express.Router();
 
 router.post("/", async (req: express.Request, res: express.Response) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Content-Type", "text/event-stream;charset=utf-8");
   res.setHeader("Cache-Control", "no-cache, no-transform");
   res.setHeader("X-Accel-Buffering", "no");
-  res.setHeader("Connection", "keep-alive");
 
   const params = validateParams(req.body);
   if (params == null) {
