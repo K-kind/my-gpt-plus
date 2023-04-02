e0fc2b5db230f90ddfe902e18da2575399c9b451
Keita Katahira <katahirakeita1201@gmail.com>
2023-04-02 17:08:40 +0900

    update: ページ遷移時にnotificationをクリアする

--- src/shared/hooks/useNotification.tsx
+++ src/shared/hooks/useNotification.tsx
@@ -4,6 +4,8 @@ import {
   IconExclamationMark,
   IconInfoSmall,
 } from "@tabler/icons-react";
+import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
 
 type OptionsForSuccess = {
   title?: string;
@@ -18,17 +20,26 @@ type OptionsForError = {
 };
 
 export const useNotification = () => {
+  const longRemainingIds = useRef<string[]>([]);
+
   const notifySuccess = ({
     title,
     message,
     options = {},
   }: OptionsForSuccess) => {
+    let id;
+    if (options.autoClose === false || typeof options.autoClose === "number") {
+      id = getRandomId();
+      longRemainingIds.current.push(id);
+    }
+
     notifications.show({
       title,
       message,
       icon: <IconCheck size={18} />,
       color: "teal",
       styles: { icon: { height: 24, width: 24 } },
+      id,
       ...options,
     });
   };
@@ -38,26 +49,52 @@ export const useNotification = () => {
     message = "エラーが発生しました。",
     options = {},
   }: OptionsForError = {}) => {
+    let id;
+    if (options.autoClose === false || typeof options.autoClose === "number") {
+      id = getRandomId();
+      longRemainingIds.current.push(id);
+    }
+
     notifications.show({
       title,
       message,
       icon: <IconExclamationMark size={18} />,
       color: "red",
       styles: { icon: { height: 24, width: 24 } },
+      id,
       ...options,
     });
   };
 
   const notifyInfo = ({ title, message, options = {} }: OptionsForSuccess) => {
+    let id;
+    if (options.autoClose === false || typeof options.autoClose === "number") {
+      id = getRandomId();
+      longRemainingIds.current.push(id);
+    }
+
     notifications.show({
       title,
       message,
       icon: <IconInfoSmall size={18} />,
       color: "blue",
       styles: { icon: { height: 24, width: 24 } },
+      id,
       ...options,
     });
   };
+
+  const router = useRouter();
+
+  // すぐに閉じない通知がページ遷移時にまだ残っていた場合、閉じる
+  useEffect(() => {
+    return () => {
+      for (const id of longRemainingIds.current) {
+        notifications.hide(id);
+      }
+      longRemainingIds.current = [];
+    };
+  }, [router.asPath]);
 
   return {
     notifySuccess,
@@ -65,3 +102,5 @@ export const useNotification = () => {
     notifyInfo,
   };
 };
+
+const getRandomId = () => Math.random().toString(32).substring(2);
