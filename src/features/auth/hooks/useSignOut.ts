a94b915f5b2248d89fe82a8ba8e8c7a402a61c94
Keita Katahira <katahirakeita1201@gmail.com>
2023-04-03 22:53:15 +0900

    ログアウト時にキャッシュクリア

--- src/features/auth/hooks/useSignOut.ts
+++ src/features/auth/hooks/useSignOut.ts
@@ -1,9 +1,14 @@
 import { signOut } from "@/features/auth/api/signOut";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export const useSignOut = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     // ログアウト後すぐにsetUser(null)すると弊害があるため、setUserは使用側で行う
     mutationFn: signOut,
+    onSuccess: () => {
+      queryClient.clear();
+    },
   });
 };
