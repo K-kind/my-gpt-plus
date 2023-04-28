331e9030bf9fc8e6309c5f092b130fdf749490c2
Keita Katahira <katahirakeita1201@gmail.com>
2023-04-29 08:50:45 +0900

    update: GPT-4に関する紹介を追加

--- src/features/static/components/HomePage.tsx
+++ src/features/static/components/HomePage.tsx
@@ -19,7 +19,7 @@ export const HomePage = () => {
         <List>
           <List.Item>質問の事前指示のセット・管理ができる</List.Item>
           <List.Item>チャット履歴を確認・検索できる</List.Item>
-          <List.Item>高速なGPT-3.5（Turbo）モデルを使用</List.Item>
+          <List.Item>GPT-3.5（Turbo）モデル、GPT-4モデルが使える</List.Item>
         </List>
       </ItemWithTitle>
 
